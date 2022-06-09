965b9ffc63b2fe73ba3f913e4d710efca7b935b5
Buushe <aadangoranyo@gmail.com>
2022-06-09 05:07:32 -0700

    task 2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,33 +4,7 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-#include "main.h"
-
-/**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
-
 int largest_number(int a, int b, int c)
-{
-	int largest;
+void print_remaining_days(int month, int day, int year)
 
-	if (a >= b && a >= c)
-	{
-		largest = a;
-	}
-	else if (b >= a && b >= c)
-	{
-		largest = b;
-	}
-	else
-	{
-		largest = c;
-	}
-
-	return (largest);
-}
 #endif /* MAIN_H */
