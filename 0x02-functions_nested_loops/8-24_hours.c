91f304f6557a7b139ba3cc816353c0a2564c9b69
Buushe <aadangoranyo@gmail.com>
2022-06-08 21:11:04 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * jack_bauer - prints time table in this format: HH:MM
+ * jack_bauer -> prints time table in this format: HH:MM
  * Return: the sum of two numbers.
  */
-void jack_bauer(void)
-{
+void jack_bauer(void){
 	int h, m;
 
 	for (h = 0; h < 24; h++)
