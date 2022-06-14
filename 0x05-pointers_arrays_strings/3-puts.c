91362ff1a85faa1d65246c7fd75a71335320955a
Buushe <aadangoranyo@gmail.com>
2022-06-14 10:50:23 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * print_rev - print reverse characters.
- * @s: validate the character
- * Return: Always 0.
+ * _puts - a function that prints a string
+ * @str: string input
+ * Return: string
  */
 void _puts(char *str)
 {
-        for (; i >= 0; i--)
-        {
-                _putchar(*str);
-        }
-        _putchar('\n');
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
